1363ce3bcdfae3705a80d7c6542af4cd5e4f882f
Jerry Aldrich <jerryaldrichiii@gmail.com>
2018-02-27 13:23:38 -1000

    WIP
    Signed-off-by: Jerry Aldrich <jerryaldrichiii@gmail.com>

--- test/unit/utils/find_files_test.rb
+++ test/unit/utils/find_files_test.rb
@@ -14,10 +14,37 @@ describe FindFiles do
     FindFilesTest.new
   end
 
+  let(:inspec) { mock }
+  let(:result) { mock }
+  let(:shim) { findfiles }
+
   describe '#find_files' do
-    it 'returns an array (of findings)' do
-      files = findfiles.find_files('/no/such/mock', type: 'file', depth: 1)
-      files.must_equal([])
+    it 'returns an empty array when no files are found' do
+      shim.expects(:warn)
+      shim.find_files('/no/such/mock', type: 'file', depth: 1).must_equal([])
+    end
+  end
+
+  describe '#find_files_or_warn' do
+    before do
+      shim.expects(:inspec).returns(inspec)
+      result.stubs(:exit_status).returns(0)
+      result.stubs(:stdout).returns('mock')
+    end
+
+    it 'constructs the correct command' do
+      inspec.expects(:command).with("sh -c 'find /foo/bar/'").returns(result)
+      shim.find_files('/foo/bar/')
+    end
+
+    it 'constructs the correct command when a single quote is in the path' do
+      inspec.expects(:command).with(%{sh -c "find /foo/\'bar/"}).returns(result)
+      shim.find_files(%{/foo/'bar/})
+    end
+
+    it 'constructs the correct command when a double quote is in the path' do
+      inspec.expects(:command).with(%{sh -c 'find /foo/\"bar/'}).returns(result)
+      shim.find_files(%{/foo/"bar/})
     end
   end
 end
